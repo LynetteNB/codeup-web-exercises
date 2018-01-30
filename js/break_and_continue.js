e31887428eaa91fdf42a1495480ba9fbdf2f636f
Lynette <lyna8907@gmail.com>
2018-01-30 15:08:36 -0600

    worked on bonus loop exercises

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -20,3 +20,40 @@ function skipNumber() {
         }
     }
 }
+
+// BONUSES 1:
+// Write a function to toggle all characters in a string: in a given string, toggle all characters to their opposite case. Special characters will not change.
+//     Example input: "aBc12#"
+// Example output: "AbC12#"
+function toggleCase(string) {
+    var newString = "";
+    for(var i = 0; i < string.length; i++) {
+        if(string[i] == string[i].toLowerCase()){
+            newString += string[i].toUpperCase();
+        } else if (string[i] == string[i].toUpperCase()) {
+            newString += string[i].toLowerCase();
+        }
+    }
+    return newString;
+
+}
+//
+
+// BONUS 2:
+// Write a function that takes a given string appended with a number at the end. The program will check the length of the string to verify the string is
+// equal to the number appended. If the number appended equals the string length, output "Yes". if the number appended does not equal the string length,
+// output "No".
+//     Example Input: "geek5"
+// Example Output: "Yes"
+// Example Input: "codingchallenge25"
+// Example Output: "No"
+function checkStringLength(string) {
+    var number = "";
+    for(var i = 0; i < string.length; i++) {
+        if (isNaN(string[i]) == false) {
+            number = string.substring(i, string.length);
+            break;
+        }
+    }
+    return number == string.length ? "Yes" : "No";
+}
