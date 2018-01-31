255e7e081c3975b8f6bf5d09496b0d01b9033dd8
Lynette <lyna8907@gmail.com>
2018-01-30 18:04:13 -0600

    changed skipNumber function to have nested loop

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -1,22 +1,20 @@
 function skipNumber() {
-    var number = prompt("Please enter an odd number from 1-50");
-    while (isNaN(number) || number > 50 || number < 1 || number%2 == 0) {
+    var number;
+    while (number = prompt("Please enter an odd number from 1-50")) {
         if (number == null) {
             break;
-        }
-        if (number >=1 && number <=50){
-            break;
-        }
-        alert("Please enter a valid odd number!");
-        number = prompt("Please enter an odd number from 1-50");
-    }
-    for(var i = 1; i <= 50 && number; i++) {
-        if (i == number) {
-            console.log("Yikes! Skipping this number: " + i);
-            continue;
-        }
-        if (i%2==1) {
-            console.log("Here is an odd number: " + i)
+        } else if (number >=1 && number <=50 && number%2==1){
+            for(var i = 1; i <= 50 && number; i++) {
+                if (i == number) {
+                    console.log("Yikes! Skipping this number: " + i);
+                    continue;
+                }
+                if (i%2==1) {
+                    console.log("Here is an odd number: " + i)
+                }
+            } break;
+        } else {
+            alert("Please enter a valid odd number!");;
         }
     }
 }
