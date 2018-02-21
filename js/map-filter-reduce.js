3837e425f2d3e83b361ab0e1d6b1b3bbdaa91482
Lynette <lyna8907@gmail.com>
2018-02-20 21:51:30 -0600

    map filter reduce exercise complete, simon says functionality done but not flashing correctly

---
+++ js/map-filter-reduce.js
@@ -0,0 +1,44 @@
+const users = [
+    {
+        id: 1,
+        name: 'ryan',
+        email: 'ryan@codeup.com',
+        languages: ['clojure', 'javascript'],
+    },
+    {
+        id: 2,
+        name: 'luis',
+        email: 'luis@codeup.com',
+        languages: ['java', 'scala', 'php'],
+    },
+    {
+        id: 3,
+        name: 'zach',
+        email: 'zach@codeup.com',
+        languages: ['javascript', 'bash'],
+    },
+    {
+        id: 4,
+        name: 'fernando',
+        email: 'fernando@codeup.com',
+        languages: ['java', 'php', 'sql'],
+    },
+    {
+        id: 5,
+        name: 'justin',
+        email: 'justin@codeup.com',
+        languages: ['html', 'css', 'javascript', 'php'],
+    },
+];
+
+users.filter(user => {
+   if(user.languages.length >= 3){
+       return user;
+   }
+});
+users.map(user => user.email);
+users.reduce((accum, user) => {
+    let {id, name, email, languages} = user;
+    accum[id] = {name, email, languages};
+    return accum;
+}, {});
