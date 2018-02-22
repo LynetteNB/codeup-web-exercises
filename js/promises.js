dca2400c28d2aa1348c9a5d7e8c99a4eb25152bb
Lynette <lyna8907@gmail.com>
2018-02-22 09:53:37 -0600

    changes to promises exercise

--- js/promises.js
+++ js/promises.js
@@ -4,10 +4,9 @@ let wait = (time) => {
                 resolve();
             }, time)});
 };
-//github api Personal Access Token: a46e864b55c7b97d68800deed2edf493fc7a94d3
-
 
 let lastLogin = (username) => {
     let gitHubPromise = fetch(`https://api.github.com/users/${username}/events`, {headers: {'Authorization': 'a46e864b55c7b97d68800deed2edf493fc7a94d3'}});
     return gitHubPromise.then(response => response.json()).then(data => {return data[0].created_at});
 };
+lastLogin("lynettenb").then(date => console.log(`The date of the last commit is ${date}`));
